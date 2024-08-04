f7d8784d59a56a8e15a553fae187137461a0d69c
forgebin <109853811+forgebin@users.noreply.github.com>
2024-08-04 16:38:43 +0300

    Create keysys.lua

---
+++ keysys.lua
@@ -0,0 +1,57 @@
+local RobloxFrameGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local TextBox = Instance.new("TextBox")
+local UICorner_2 = Instance.new("UICorner")
+local TextLabel = Instance.new("TextLabel")
+local UICorner_3 = Instance.new("UICorner")
+
+--Properties:
+
+RobloxFrameGui.Name = "RobloxFrameGui"
+RobloxFrameGui.Parent = game.CoreGui
+RobloxFrameGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = RobloxFrameGui
+Frame.BackgroundColor3 = Color3.fromRGB(57, 57, 57)
+Frame.BackgroundTransparency = 0.200
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.357859492, 0, 0.407114714, 0)
+Frame.Size = UDim2.new(0.284118116, 0, 0.149927214, 0)
+
+UICorner.Parent = Frame
+
+TextBox.Parent = Frame
+TextBox.BackgroundColor3 = Color3.fromRGB(31, 31, 31)
+TextBox.BackgroundTransparency = 0.200
+TextBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextBox.BorderSizePixel = 0
+TextBox.Position = UDim2.new(0.0561797768, 0, 0.448999196, 0)
+TextBox.Size = UDim2.new(0.887640476, 0, 0.330097079, 0)
+TextBox.Font = Enum.Font.Unknown
+TextBox.PlaceholderText = "LICENSE"
+TextBox.Text = ""
+TextBox.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextBox.TextSize = 14.000
+TextBox.TextXAlignment = Enum.TextXAlignment.Left
+
+UICorner_2.Parent = TextBox
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(36, 40, 65)
+TextLabel.BackgroundTransparency = 0.200
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0.219101131, 0, 0.106796116, 0)
+TextLabel.Size = UDim2.new(0.561797738, 0, 0.233009711, 0)
+TextLabel.Font = Enum.Font.Unknown
+TextLabel.Text = "Enter your license."
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+
+UICorner_3.Parent = TextLabel
+
+return RobloxFrameGui
